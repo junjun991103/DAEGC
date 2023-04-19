1ac04a232db7e6b21e8be0515039b82ca46ad212
junjun991103 <131244745+junjun991103@users.noreply.github.com>
2023-04-19 18:42:02 +0800

    Update readData.py

--- DAEGC/readData.py
+++ DAEGC/readData.py
@@ -9,10 +9,10 @@ def read_csv_cols(filename, cols):
     # 返回数组
     return data
 # 读取两个CSV文件的数据
-filename1 = '02癌症换特征加fid加编号.csv'
-# filename2 = '02合并后的污染源加fid加编号.csv'
+#filename1 = '02癌症换特征加fid加编号.csv'
+filename2 = '02合并后的污染源加fid加编号.csv'
 cols = [2, 3]  # 读取前两列作为二维坐标
-data = read_csv_cols(filename1, cols)
+data = read_csv_cols(filename2, cols)
 # data1 = read_csv_cols(filename1, cols)
 # data2 = read_csv_cols(filename2, cols)
 np.save('./mydataset',data)
